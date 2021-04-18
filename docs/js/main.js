7ef5cc00f493766d8ce858026ed9161152b3066f
amateo <amateo@atsistemas.com>
2021-04-18 19:02:53 +0200

    Navbar styles control

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,12 +1,30 @@
 // This initialization requires that this script is loaded with `defer`
-const navElement = document.querySelector('#navbar');
+const navElement = document.querySelector("#navbar");
 
 // Navigation element modification through scrolling
 function scrollFunction() {
-  if (document.documentElement.scrollTop | document.body.scrollTop > 0) {
+  if (document.documentElement.scrollTop | (document.body.scrollTop > 0)) {
     navElement.classList.add("nav-scroll");
   } else {
     navElement.classList.remove("nav-scroll");
+  }
+}
+
+function checkSize() {
+  var windSize = window.innerWidth;
+  return windSize;
+}
+
+function newsSectionStyle() {
+  const isNewSection = document.location.pathname.includes("noticias");
+  var isMediumSize = checkSize() <= 768;
+  if (isNewSection && isMediumSize) {
+    console.log('isMediumSize', isMediumSize);
+    navElement.classList.remove("pos-fixed")
+    navElement.classList.add("pos-initial");
+  } else {
+    navElement.classList.add("pos-fixed");
+    navElement.classList.remove("pos-initial");
   }
 }
 
@@ -15,6 +33,8 @@ function loadEvent() {
   document.addEventListener("scroll", scrollFunction);
   const wrapper = document.getElementById("wrapper");
   wrapper.classList.add("show");
+  newsSectionStyle();
 }
 
 window.addEventListener("load", loadEvent);
+window.addEventListener('resize', newsSectionStyle);
